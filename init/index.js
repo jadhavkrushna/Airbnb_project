92fe92a0af50bb41490dee40cd8b52c1955d1cfa
jadhavkrushna <jksaavan0055@gmail.com>
2026-03-07 09:54:34 +0530

    Enhance: Update seeding script to support Atlas connection string

--- init/index.js
+++ init/index.js
@@ -2,25 +2,30 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config({ path: "../.env" });
+}
+
+const MONGO_URL = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-.then( () =>{
-    console.log("connected to DB");
-})
-.catch((err) =>{
-    console.log(err);
-});
+    .then(() => {
+        console.log("connected to DB");
+        initDB();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-async function main(){
-   await mongoose.connect(MONGO_URL);
+async function main() {
+    await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) =>({ ...obj, owner: "684e98020ba691cb96a3f04d" }));
-   await Listing.insertMany(initData.data);
-   console.log("Data was Initialize");
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "684e98020ba691cb96a3f04d" }));
+    await Listing.insertMany(initData.data);
+    console.log("Data was Initialize");
 }
 
 initDB();
